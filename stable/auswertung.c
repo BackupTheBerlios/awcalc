0120d44210c0168a58f89ce1117a3605ba6895ac
sirro <sirro>
2004-02-27 16:51:00 +0000

    changed algorithm

--- stable/auswertung.c
+++ stable/auswertung.c
@@ -22,17 +22,37 @@
  *      Boris Wachtmeister  < sirro 'at`  nurfuerspam `dot' de >
  * ***** END LICENSE BLOCK ***** */
 
+#include<stdio.h>
+#include "header/main.h"
+#include "header/ausgabe.h"
+#include "header/strings.h"
 
-#include "header/auswertung.h"
-
-void auswertung(float result[],float wins[],float equals[],float loose[],int high)
+void ausgabe(direktvergleich **dv_tabelle,int *platzierungen,int *bewertungen,float *ergebnis,float *siege,float *niederlagen,float *unentschieden, int spalten, int zeilen)
 {
-    /*          2*Sieganzahl + Zahl der Unentschieden
-      -----------------------------------------------------------
-       2 * (Sieganzahl + Verlustanzahl + Zahl der Unentschieden)	    */
-    int i;
-    
-    for(i=0;i<high;i++)
-	result[i]=(2*wins[i]+equals[i])/2/(wins[i]+loose[i]+equals[i]);
-}
+	FILE *outfile;
+	int i,j;
+	
+        outfile=fopen(OUTFILE,"w");
+        fprintf(outfile,";");
+        for(i=0;i<spalten;i++)
+                fprintf(outfile,"Ding%d;",i+1);
+        fprintf(outfile,"\n");
+        for(i=0;i<spalten;i++)
+        {
+                fprintf(outfile,"Ding%d;",i+1);
+                for(j=0;j<spalten;j++)
+                {
+                        fprintf(outfile,"%d/%d/%d;",dv_tabelle[i][j].siege,dv_tabelle[i][j].unentschieden,dv_tabelle[i][j].niederlagen);
+                }
+                fprintf(outfile,"\n");
+        }
+        fclose(outfile);
 
+	printf(LINE);
+	printf(HEADOFTABLE);
+	printf(LINE);
+	
+	for(zeilen=0;zeilen<spalten;zeilen++)
+		printf(DATAROW,zeilen+1,siege[zeilen],unentschieden[zeilen],niederlagen[zeilen],bewertungen[zeilen],ergebnis[zeilen],platzierungen[zeilen]+1);
+	printf(LINE);
+}
